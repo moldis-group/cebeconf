43a257865593f53b501da0b5087fbda04d8cfd53
raghuramakrishnan <ramakrishnan@tifrh.res.in>
2024-01-11 19:22:17 +0530

    rr: maximum neighbors added as an option

--- test/calc.py
+++ test/calc.py
@@ -1,13 +1,11 @@
 from cebeconf import calc_be
 
-calc_be('glucose_chain_UFF.xyz')
-
+# Run with default settings
+# calc_be('glucose_chain_UFF.xyz')
 
 # Default maximum neightbors for large system
-Max_neigh={"MaxN_C":16, "MaxN_N":12, "MaxN_O":8, "MaxN_F":4}
-calc_be('glucose_chain_UFF.xyz', **Max_neigh)
+# Max_neigh={"MaxN_C":16, "MaxN_N":12, "MaxN_O":8, "MaxN_F":4}
 
-for N_F in range(1,5):
-    Max_neigh={"MaxN_C":16, "MaxN_N":12, "MaxN_O":8, "MaxN_F":N_F}
-    print(Max_neigh)
-    calc_be('glucose_chain_UFF.xyz', **Max_neigh)
+# Change the number of neighbors for F
+Max_neigh={"MaxN_C":16, "MaxN_N":12, "MaxN_O":8, "MaxN_F":2}
+calc_be('glucose_chain_UFF.xyz', **Max_neigh)
